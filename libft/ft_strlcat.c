e13e549e9e42b9a515293a88a5c520d1ac04211c
Skall77 <102985961+Skall77@users.noreply.github.com>
2022-11-09 17:25:11 +0100

    Add files via upload

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaudevar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 18:37:58 by aaudevar          #+#    #+#             */
+/*   Updated: 2022/11/07 18:46:40 by aaudevar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_len;
+	size_t	dst_len;
+
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (!size)
+		return (src_len);
+	while (src[i] && dst_len + i < size - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	if (dst_len > size)
+		return (src_len + size);
+	return (dst_len + src_len);
+}
